602f71a4dbf351c38cf6ac17f07d1cbaf152562c
KOSASIH <kosasihg88@gmail.com>
2025-02-19 13:36:16 +0700

    Create userController.js

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.registerUser  = async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).send({ message: 'User  registered successfully' });
+};
+
+exports.loginUser  = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).send({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.send({ token });
+};
